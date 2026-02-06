bddd52999b3d8ce3beacccdb0b5e3f2475ff8b13
Ramkumar V <ramkumarvenkatesan02@gmail.com>
2026-02-06 21:53:18 +0530

    sample operation

--- function&string.js
+++ function&string.js
@@ -75,3 +75,75 @@ console.log (b && a)
 console.log(`Hello, ${name || 'visitor'}!`)
 }
 greet('Logesh') 
+
+function capitalize(text) {
+  if (!text) return "";
+  return text.charAt(0).toUpperCase() + text.slice(1);
+}
+
+
+function reverseString(text) {
+  return text.split("").reverse().join("");
+}
+
+
+function countCharacter(text, char) {
+  let count = 0;
+
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] === char) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+
+function isPalindrome(text) {
+  const cleaned = text.toLowerCase().replace(/[^a-z0-9]/g, "");
+  return cleaned === reverseString(cleaned);
+}
+
+console.log(capitalize("javascript"));
+console.log(reverseString("github"));
+console.log(countCharacter("hello world", "l"));
+console.log(isPalindrome("Racecar"));
+
+
+module.exports = {
+  capitalize,
+  reverseString,
+  countCharacter,
+  isPalindrome
+};
+
+
+
+function combineNames(firstName, lastName) {
+  return firstName + " " + lastName;
+}
+
+function checkWordLength(word) {
+  if (word.length > 5) {
+    return "Long word";
+  } else {
+    return "Short word";
+  }
+}
+
+function repeatWord(word, times) {
+  let result = "";
+
+  for (let i = 0; i < times; i++) {
+    result += word;
+  }
+
+  return result;
+}
+
+
+console.log(combineNames("John", "Doe"));
+console.log(checkWordLength("JavaScript"));
+console.log(checkWordLength("Code"));
+console.log(repeatWord("Hi ", 3));
