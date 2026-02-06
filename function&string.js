5c7553edaa837f7f32afdf15f2d31195c8afeef9
Ramkumar V <ramkumarvenkatesan02@gmail.com>
2026-02-06 21:40:09 +0530

    function and string

---
+++ function&string.js
@@ -0,0 +1,77 @@
+boy = "your teeth is so clean"
+girl = "go and brush your teeth"
+
+function teeth(yes,no) {
+
+if (yes){
+    console.log(boy)
+}
+else {
+    console.log(girl)
+}
+}
+
+teeth()
+
+var season = "spring"
+
+if (season == "spring"){
+
+    console.log("enjoy the blooming flowers")
+}
+
+if (season == "summer"){
+    console.log("have a fun in sun")
+}
+
+if (season=="autumn"){
+    console.log("admire the colour leaves")
+}
+
+if (season=="winter"){
+    console.log("bundle and stay warm")
+}
+
+const name = "ramkumar"
+
+function heroes() {
+    const name = "donesh is hero"
+   if (true) {
+    const name = "nirmal form nif"
+    console.log(name)
+   } 
+    console.log(name)
+}
+console.log(name)
+heroes()
+
+
+function donesh() {
+    if (true){
+        var name = "donesh india"
+    }
+    console.log(name)
+}
+donesh()
+
+
+function hosting() {
+    a=10
+    let b= 24
+}
+hosting()    
+
+console.log(a)
+
+
+const a = 42
+const b = "donesh"
+const c = null
+
+console.log (b && a)
+
+
+ function greet(name) {
+console.log(`Hello, ${name || 'visitor'}!`)
+}
+greet('Logesh') 
